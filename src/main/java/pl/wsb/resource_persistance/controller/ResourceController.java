8b8ab1b1a9bf6522cc7c034469173b2965ef70c4
Łukasz Styk <styklukasz@gmail.com>
2025-06-13 09:55:51 +0200

    added methods

--- src/main/java/pl/wsb/resource_persistance/controller/ResourceController.java
+++ src/main/java/pl/wsb/resource_persistance/controller/ResourceController.java
@@ -1,11 +1,14 @@
 package pl.wsb.resource_persistance.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pl.wsb.resource_persistance.model.Resource;
 import pl.wsb.resource_persistance.repository.ResourceRepository;
 
+import java.lang.reflect.Field;
+import java.net.URI;
 import java.util.List;
 
 @Controller
@@ -15,17 +18,42 @@ public class ResourceController {
     @Autowired
     private ResourceRepository resourceRepository;
 
-//    @GetMapping
-//    public String listResources(Model model) {
-//        List<Resource> resources = resourceRepository.findAll();
-//        model.addAttribute("resources", resources);
-//        return "resources";
-//    }
-
     @PostMapping
     @ResponseBody
-    public Resource createResource(@RequestBody Resource resource) {
-        return resourceRepository.save(resource);
+    public ResponseEntity<?> createResource(@RequestBody Resource resource) {
+        Resource savedResource = resourceRepository.save(resource);
+        URI location = URI.create("/resources/" + savedResource.getId());
+        return ResponseEntity.created(location).body(savedResource);
+    }
+
+    @PutMapping
+    @ResponseBody
+    public ResponseEntity<?> increaseResource(@PathVariable long resourceId) throws Exception {
+        Resource resource = resourceRepository.findById(resourceId).orElseThrow(() -> new Exception("ups"));
+        Field[] fields = Resource.class.getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            try {
+                if (field.getType() == double.class) {
+                    double currentValue = field.getDouble(resource);
+                    field.setDouble(resource, currentValue + 0.1);
+                } else if (field.getType() == int.class) {
+                    int currentValue = field.getInt(resource);
+                    field.setInt(resource, currentValue + 1);
+                }
+            } catch (IllegalAccessException e) {
+                throw new Exception("ups");
+            }
+        }
+        return ResponseEntity.accepted().body(resourceRepository.save(resource));
+    }
+
+    @PutMapping
+    @ResponseBody
+    public ResponseEntity<?> reverseResourceFlags(@PathVariable long resourceId) throws Exception {
+        Resource resource = resourceRepository.findById(resourceId).orElseThrow(() -> new Exception("ups"));
+        resource.setCzyPosiadaRadio(!resource.isCzyPosiadaRadio());
+        return ResponseEntity.accepted().body(resourceRepository.save(resource));
     }
 
     @GetMapping("/api")
